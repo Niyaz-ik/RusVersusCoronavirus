a8f37f4f953aae6f15ed6a0237d012c116c7d185
Amil <Limash76912@mail.ru>
2020-05-12 11:44:36 +0300

    tables added

--- js/main.js
+++ js/main.js
@@ -1,70 +1,24 @@
-data = [{
-        amountOfDays: "12",
-        covidState: "true",
-        patientState: "normal",
-        hash: "1ed32df45gw2"
-    },
-    {
-        amountOfDays: "0",
-        covidState: "false",
-        patientState: "normal",
-        hash: "1ed32df45gw3"
-    },
-    {
-        amountOfDays: "30",
-        covidState: "true",
-        patientState: "died",
-        hash: "1ed32df45gw4"
-    },
-    {
-        amountOfDays: "2",
-        covidState: "true",
-        patientState: "normal",
-        hash: "1ed32df45gw5"
-    }
-]
-let mas = [
-    ["row 1, cell 1", "row 1, cell 2"],
-    ["row 2, cell 1", "row 2, cell 2"]
+"use strict"
+
+let mass = [
+    ["1123123123123", "3", "Amil"],
+    ["2", "433333333", "Iluxa"]
 ];
 
 
+function makeTableHTML(myArray) {
+    var result = "<table border=1>";
+    for (var i = 0; i < myArray.length; i++) {
+        result += "<tr>";
+        for (var j = 0; j < myArray[i].length; j++) {
+            result += "<td>" + myArray[i][j] + "</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</table>";
 
-function createTable(tableData) {
-    // const contentElement = document.querySelector("#root");
-    var table = document.createElement('table');
-    var tableBody = document.createElement('tbody');
-
-    tableData.forEach(function (rowData) {
-        var row = document.createElement('tr');
-
-        rowData.forEach(function (cellData) {
-            var cell = document.createElement('td');
-            cell.appendChild(document.createTextNode(cellData));
-            row.appendChild(cell);
-        });
-
-        tableBody.appendChild(row);
-    });
-
-    table.appendChild(tableBody);
-    document.body.appendChild(table);
+    return result;
 }
-
-
-// function makeTableHTML(myArray) {
-//     var result = "<table border=1>";
-//     for (var i = 0; i < myArray.length; i++) {
-//         result += "<tr>";
-//         for (var j = 0; j < myArray[i].length; j++) {
-//             result += "<td>" + myArray[i][j] + "</td>";
-//         }
-//         result += "</tr>";
-//     }
-//     result += "</table>";
-
-//     return result;
-// }
-
-const contentElement = document.querySelector("#root");
-contentElement.innerHTML = createTable(mas);;
+let markup = makeTableHTML(mass);
+let elem = document.getElementById("table");
+elem.insertAdjacentHTML("beforeend", markup);
